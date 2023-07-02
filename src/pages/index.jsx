20d88a2879ee9daaeae279dd469f29e77b0a5841
miguel mateo <mmateo95@hotmail.com>
2023-07-02 18:50:44 -0400

    created error and register page and expoted from index.jsx file

---
+++ src/pages/index.jsx
@@ -0,0 +1,8 @@
+import Dashboard from "./Dashboard";
+import Error from "./Error";
+import LandingPage from "./LandingPage";
+import Register from "./Register";
+
+export {
+Register, Error, LandingPage, Dashboard,
+}
