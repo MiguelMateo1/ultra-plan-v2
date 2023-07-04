b42abb5d47c836826268d71233d31bdb1346ee1c
miguel mateo <mmateo95@hotmail.com>
2023-07-03 22:29:03 -0400

    created server folder for back-end and moved front-end file into client folder

--- src/components/index.js
+++
@@ -1,7 +0,0 @@
-import Logo from './Logo';
-import LandingInfo from './LandingInfo';
-import FormRow from './FormRow';
-
-export {
-  Logo, LandingInfo, FormRow
-};
--- src/pages/index.jsx
+++
@@ -1,8 +0,0 @@
-import Dashboard from "./Dashboard";
-import Error from "./Error";
-import LandingPage from "./LandingPage";
-import Register from "./Register";
-
-export {
-Register, Error, LandingPage, Dashboard,
-}
