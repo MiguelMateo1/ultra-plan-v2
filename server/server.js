cc2c7344458ec5ff5518ded308b4b407fd745e11
miguel mateo <mmateo95@hotmail.com>
2023-07-06 18:19:36 -0400

    connedted to database and logic for login and register added

--- server/server.js
+++ server/server.js
@@ -1,12 +1,84 @@
 const express = require('express');
-const app = express();
+const mysql = require('mysql');
 const cors = require('cors');
 
+const app = express();
 app.use(cors());  
+app.use(express.json());
 
-app.get('/api', (req, res) => {
-    res.json({'users': ['userOne', 'userTwo', 'userThree']})
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'test'
+});
+
+// Connect to the database
+db.connect((err) => {
+    if (err) {
+      console.error('Error connecting to the database: ' + err);
+      return;
+    }
+    console.log('Connected to the database');
+  });
+
+// login user 
+app.post('/api/login', (req, res) => {
+    const { email, password } = req.body;
+    const sql = "SELECT * FROM app_users WHERE email = ? AND password = ?";
+
+    db.query(sql, [email, password], (err, result) => {
+        if(err) {
+            console.error('Error executing the query: ' + err);
+            return res.status(500).json({message: "'Internal server error"})
+        }
+        console.log(result)
+        if (result.length > 0) {
+            // User exists
+          } else {
+            return res.json({ message: 'Invalid email or password' });
+          }
+    })
+});
+// login user End=========
+
+// register user 
+app.post('/api/register', (req, res) => {
+    const {first_name, last_name, email, password} = req.body;
+
+    // checks if user exist
+    const checkSql = "SELECT * FROM app_users WHERE email = ?";
+    db.query(checkSql, [email,], (err, result) => {
+        if (result.length > 0) {
+            return res.json({ message: 'User already exists' });
+     // checks if user exist END====
+        } else {
+            // register new user
+            const sql = "INSERT INTO app_users(first_name, last_name, email, password) VALUES (?,?,?,?)";
+            db.query(sql, [first_name, last_name ,email, password], (err, result) => {
+                if(err) {
+                    return res.json({message: "error"})
+                }
+                // user created 
+                return res.json(result);
+            })
+        }
+    })
 })
+// register user End====
+
+// test get user**
+app.get('/:first_name', (req, res) => {
+    const sqlSelect = "SELECT * FROM app_users WHERE email = ? AND first_name = ?";
+    const val = req.params;
+
+    db.query(sqlSelect, [val.first_name, val.email], (err, result) => {
+        if(err) {
+            return res.json({Message: "error error"})
+        }
+        return res.json(result);
+    })
+});
 
 app.listen(8000, () => {
     console.log('server started')
