7f191888828a98c87b0be34acc297994cdbc659d
miguel mateo <mmateo95@hotmail.com>
2023-07-08 10:44:44 -0400

    imported dashboard pages and added wrap in protectedRoute

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
-import {LandingPage, Error, Dashboard, Register} from "./pages"
+import { LandingPage, Error, Register, ProtectedRoute} from "./pages"
+import { Profile, AddSkill, Stats, Layout, MySkills } from './pages/dashboard';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
@@ -9,13 +10,23 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Dashboard />} />
+      
+        <Route path="/" element={
+          <ProtectedRoute>
+            <Layout />
+          </ProtectedRoute>} >
+            <Route index element={<Stats />} />
+            <Route path='my-skills' element={<MySkills />} />
+            <Route path='add-skill' element={<AddSkill />} />
+            <Route path='profile' element={<Profile />} />
+        </Route>
+
         <Route path="landing" element={<LandingPage />} />
         <Route path="register" element={<Register />} />
         <Route path="/*" element={<Error />} />
       </Routes>
       {/* toast use for popup alerts */}
-      <ToastContainer position="top-center" autoClose={3500} />
+      <ToastContainer position="top-center" autoClose={3000} />
     </BrowserRouter>
   )
 }
