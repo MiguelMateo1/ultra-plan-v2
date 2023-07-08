5123c815b514d9d267e29578bf060590a399fbc4
miguel mateo <mmateo95@hotmail.com>
2023-07-08 10:55:33 -0400

    created and exported ProtectedRoure file

--- client/src/pages/index.jsx
+++ client/src/pages/index.jsx
@@ -1,8 +1,8 @@
-import Dashboard from "./Dashboard";
 import Error from "./Error";
 import LandingPage from "./LandingPage";
+import ProtectedRoute from "./ProtectedRoute";
 import Register from "./Register";
 
 export {
-Register, Error, LandingPage, Dashboard,
-}
+Register, Error, LandingPage, ProtectedRoute
+};
