e20734974728174da87d97dc8aaef55c99e021db
miguel mateo <mmateo95@hotmail.com>
2023-07-09 11:46:33 -0400

    move register page css to syles folder

---
+++ client/src/assets/styles/RegisterCSS.js
@@ -0,0 +1,42 @@
+import styled from 'styled-components'
+
+// css styles
+const Styles = styled.section`
+  display: grid;
+  align-items: center;
+  min-height: 100vh;
+  .logo {
+    width: 60px;
+    display: block;
+    margin: 0 auto;
+    margin-bottom: 1.3rem;
+  }
+  .form {
+    max-width: 400px;
+    border-top: 5px solid var(--primary-3);
+  }
+
+  h3 {
+    text-align: center;
+  }
+  p {
+    margin: 0;
+    margin-top: 1rem;
+    text-align: center;
+  }
+  .btn {
+    margin-top: 1rem;
+    transition: var(--transition);
+  }
+  .btn:hover {
+    background-color: var(--primary-3);
+  }
+  .member-btn {
+    background: transparent;
+    border: transparent;
+    color: var(--secondary-4);
+    cursor: pointer;
+    letter-spacing: var(--letterSpacing);
+  }
+`
+export default Styles;
