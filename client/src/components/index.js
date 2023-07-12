6d3a2b3e715a1d81826f1ed58e5cdec1ceba516e
miguel mateo <mmateo95@hotmail.com>
2023-07-12 16:28:37 -0400

    created form select component and exported files

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,7 +1,11 @@
 import Logo from './Logo';
 import LandingInfo from './LandingInfo';
 import FormRow from './FormRow';
+import Navbar from './Navbar';
+import SmallSidebar from './SmallSidebar';
+import BigSidebar from './BigSidebar';
+import FormRowSelect from './FormRowSelect';
 
 export {
-  Logo, LandingInfo, FormRow
+  Logo, LandingInfo, FormRow, Navbar, SmallSidebar, BigSidebar, FormRowSelect
 };
