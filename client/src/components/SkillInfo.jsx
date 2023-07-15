29ae306a67c741619577e40db95808fe8ea65ce6
miguel mateo <mmateo95@hotmail.com>
2023-07-14 21:59:29 -0400

    updated return, added title and a class prop

--- client/src/components/SkillInfo.jsx
+++ client/src/components/SkillInfo.jsx
@@ -1,10 +1,11 @@
 import Styles from '../assets/styles/SkillInfoCSS';
 
-const JobInfo = ({ icon, text }) => {
+const JobInfo = ({ icon, text, title, c }) => {
   return (
     <Styles>
+      <h6>{title}</h6>
       <span className='icon'>{icon} </span>
-      <span className='text'>{text} </span>
+      <span className={`text ${c}`}>{text} </span>
     </Styles>
   );
 };
