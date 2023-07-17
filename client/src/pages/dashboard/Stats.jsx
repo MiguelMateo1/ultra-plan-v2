e1e4251fe2f500a1bc562a83ddbf0b01c3f1517c
miguel mateo <mmateo95@hotmail.com>
2023-07-16 23:43:52 -0400

    updated page/imports returns, main setup complet

--- client/src/pages/dashboard/Stats.jsx
+++ client/src/pages/dashboard/Stats.jsx
@@ -1,10 +1,35 @@
-const Stats = () => {
+import { useEffect } from 'react';
+import { StatsContainer, ChartsContainer } from '../../components';
+import { useDispatch, useSelector } from 'react-redux';
+import { getAllSkills, showStats } from '../../features/allSkills/allSkillsSlice';
+import LogHours from '../../components/LogHours';
 
-    return (
-        <div>
-          <h1>Stats</h1>
-        </div>
-    );
-  };
+const Stats = () => {
+  const {isLoading, stats, skills} = useSelector(
+    (store) => store.allSkills
+  );
+  const { user } = useSelector(
+    (store) => store.user
+  );
+
+
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getAllSkills(user.id));
+    dispatch(showStats(user.id));
+  }, []);
+
+  if (isLoading) {
+    return <h2>Loading...</h2>
+
+  }
+  return (
+    <>
+      <LogHours />
+      <StatsContainer />
+      <ChartsContainer />
+    </>
+  );
+};
 
   export default Stats;
