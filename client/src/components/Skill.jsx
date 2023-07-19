57d0fd2179905255db1c08fd2abb446368af7681
miguel mateo <mmateo95@hotmail.com>
2023-07-18 21:45:06 -0400

    added modal on btn click to confirm and added more icons

--- client/src/components/Skill.jsx
+++ client/src/components/Skill.jsx
@@ -1,17 +1,23 @@
-import { FaLocationArrow, FaCalendarAlt, FaCalendarWeek, FaHourglassHalf, FaClipboardList } from 'react-icons/fa';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import Styles from '../assets/styles/SkillCSS';
 import { useDispatch } from 'react-redux';
 import SkillInfo from './SkillInfo';
 import { deleteSkill, setEditSkill } from '../features/skills/skillsSlice';
-import { formatDistance, subDays, add, format } from 'date-fns'
-
+import { add, format } from 'date-fns'
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Modal from '@mui/material/Modal';
+// icons
+import { FaLocationArrow, FaCalendarAlt, FaCalendarWeek, FaHourglassHalf, FaClipboardList } from 'react-icons/fa';
+import { PiPianoKeysFill, PiGuitarFill } from "react-icons/pi";
 import { 
   IoBookSharp,IoBarbellSharp,IoBasketballSharp,
   IoBicycleSharp,IoEasel,IoFlaskSharp,IoHeadsetSharp,
   IoHardwareChipSharp,IoPencilSharp,IoReaderSharp,IoSchool,
-  IoTelescopeSharp,IoTerminal,IoDesktop } 
+  IoTelescopeSharp,IoTerminal,IoDesktop,IoMusicalNotes,IoBrush } 
 from "react-icons/io5";
+
 
 const Skill = ({
   id,
@@ -37,10 +43,26 @@ const Skill = ({
   // calculate the estimated coompleteion date END=====
 
 // icon to chose from when creating a skill
-  const skillIcons = [<IoBookSharp/>,<IoBarbellSharp/>,<IoBasketballSharp/>,<IoBicycleSharp/>,
-      <IoEasel/>,<IoFlaskSharp/>,<IoHeadsetSharp/>,<IoHardwareChipSharp/>, <IoPencilSharp/>,
-      <IoReaderSharp/>,<IoSchool/>,<IoTelescopeSharp/>,<IoTerminal/>,<IoDesktop/>
+  const skillIcons = [<IoBookSharp/>,<IoBarbellSharp/>,<PiPianoKeysFill />,<PiGuitarFill />,<IoBasketballSharp/>,<IoBicycleSharp/>,
+      <IoEasel/>,<IoFlaskSharp/>,<IoHeadsetSharp/>,<IoHardwareChipSharp/>,<IoBrush />, <IoPencilSharp/>,
+      <IoReaderSharp/>,<IoSchool/>,<IoTelescopeSharp/>,<IoTerminal/>,<IoDesktop/>,<IoMusicalNotes/>
   ];
+
+  // modal
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+  const modalStyle = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 350,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+  };
 
   return (
     <Styles>
@@ -58,7 +80,10 @@ const Skill = ({
           <SkillInfo icon={<FaCalendarWeek />} text={`${days_per_week} - Days per week`} />
           <SkillInfo icon={<FaHourglassHalf />} text={`${hour_per_day} - hour per day`} />
           <SkillInfo icon={<FaClipboardList />} text={`${completed_hours} - completed hours`} c='hour' />
-          <div className={`status`}>in progress</div>
+          <div 
+            className={`status ${total_hours <= completed_hours && 'completed'}`}>
+            {total_hours <= completed_hours ? 'Completed!' : 'in progress'}
+          </div>
         </div>
         <footer>
           <div className='actions'>
@@ -80,13 +105,21 @@ const Skill = ({
             >
               Edit
             </Link>
-            <button
-              type='button'
-              className='btn delete-btn'
-              onClick={() => dispatch(deleteSkill(id))}
-            >
-              Delete
-            </button>
+            {/* delete btn and modal */}
+             <Button onClick={handleOpen} className='btn delete-btn'>Delete</Button>
+                <Modal
+                  open={open}
+                  onClose={handleClose}
+                  aria-labelledby="modal-modal-title"
+                  aria-describedby="modal-modal-description"
+                >
+                  <Box sx={modalStyle}>
+                    <h4>confirm delete skill?</h4>
+                      <Button type='button' onClick={() => dispatch(deleteSkill(id))}>
+                        Delete
+                      </Button>
+                  </Box>
+                </Modal>
           </div>
         </footer>
       </div>
