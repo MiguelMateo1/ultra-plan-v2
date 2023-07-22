6dabaca1ee2f118bb07106fcfceb1a37b4aa3155
miguel mateo <mmateo95@hotmail.com>
2023-07-21 23:13:21 -0400

    imports/exports added

--- client/src/pages/index.jsx
+++ client/src/pages/index.jsx
@@ -1,8 +1,11 @@
 import Error from "./Error";
 import LandingPage from "./LandingPage";
+import OTPInput from "./OTPInput";
+import PasswordReset from "./PasswordReset";
 import ProtectedRoute from "./ProtectedRoute";
 import Register from "./Register";
+import Reset from "./Reset";
 
 export {
-Register, Error, LandingPage, ProtectedRoute
+Register, Error, LandingPage, ProtectedRoute, PasswordReset, OTPInput, Reset
 };
--- client/src/store.jsx
+++ client/src/store.jsx
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit';
 import userSlice from './features/user/userSlice';
 import skillsSlice from './features/skills/skillsSlice';
 import allSkillsSlice from './features/allSkills/allSkillsSlice';
+import resetPasswordSlice from './features/user/resetPasswordSlice';
 
 export const store = configureStore({
     reducer: {
         user: userSlice,
         skill: skillsSlice,
-        allSkills: allSkillsSlice
+        allSkills: allSkillsSlice,
+        reset: resetPasswordSlice 
     },
 });
