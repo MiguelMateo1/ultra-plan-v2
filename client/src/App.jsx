5d5e8463e097f067d72a47418e839c14553488b2
miguel mateo <mmateo95@hotmail.com>
2023-07-21 23:14:38 -0400

    reset password pages/routes added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
-import { LandingPage, Error, Register, ProtectedRoute} from "./pages"
+import { LandingPage, Error, Register, ProtectedRoute, PasswordReset, OTPInput, Reset} from "./pages"
 import { Profile, AddSkill, Stats, Layout, MySkills } from './pages/dashboard';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
@@ -24,6 +24,10 @@ function App() {
         <Route path="landing" element={<LandingPage />} />
         <Route path="register" element={<Register />} />
         <Route path="/*" element={<Error />} />
+        {/* password reset pages */}
+        <Route path="/forgotpassword" element={<PasswordReset/>} />
+        <Route path="/otp" element={<OTPInput/>} />
+        <Route path="/otp/reset" element={<Reset/>} />
       </Routes>
       {/* toast use for popup alerts */}
       <ToastContainer position="top-center" autoClose={4000} hideProgressBar={true} pauseOnFocusLoss={false} />
