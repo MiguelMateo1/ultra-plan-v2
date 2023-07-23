5d7101edbb08815b833859445c0977ac7f79e6a0
miguel mateo <mmateo95@hotmail.com>
2023-07-23 12:23:22 -0400

    changed base url

--- client/src/utils/axios.jsx
+++ client/src/utils/axios.jsx
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const fetchUrl = axios.create({
-  baseURL: 'http://localhost:8000',
+  // baseURL: 'http://localhost:8000',
+  baseURL: 'https://ultra-plan.onrender.com',
 });
 
 export default fetchUrl;
