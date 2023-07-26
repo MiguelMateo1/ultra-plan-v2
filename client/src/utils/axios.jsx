1ca292a3098568c5ae78cacca9ef4d8398f52071
miguel mateo <mmateo95@hotmail.com>
2023-07-26 18:18:19 -0400

    fetch url updated

--- client/src/utils/axios.jsx
+++ client/src/utils/axios.jsx
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const fetchUrl = axios.create({
-  // baseURL: 'http://localhost:8000',
-  baseURL: 'https://ultra-plan.onrender.com',
+  baseURL: 'http://localhost:8000',
+  // baseURL: 'https://ultra-plan.onrender.com',
 });
 
 export default fetchUrl;
