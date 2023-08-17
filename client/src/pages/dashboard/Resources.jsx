d18b827088d83b6ffa80107a6abce5c46d7a4e3e
Miguel Mateo <mmateo95@hotmail.com>
2023-08-17 17:35:10 -0400

    updated modal styles + icon added + shorted text

--- client/src/pages/dashboard/Resources.jsx
+++ client/src/pages/dashboard/Resources.jsx
@@ -1,30 +1,58 @@
 import { useState } from 'react';
+import { RxCross1 } from "react-icons/rx"; // icon
 import styled from 'styled-components'
 import RecommendedBooks from '../../components/resources/RecommendedBooks';
 import RecommendedApps from '../../components/resources/RecommendedApps';
 import Footer from '../../components/resources/Footer';
 // modal mui/styles
-import Box from '@mui/material/Box';
-import Modal from '@mui/material/Modal';
+import { Box, Button, Modal } from '@mui/material';
+
+// modal styles
+const modalStyle = {
+  position: 'absolute',
+  minWidth: 354,
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  backgroundColor: 'white', // Adjust background color as needed
+  border: '2px solid black', // Adjust border color and style as needed
+  boxShadow: 24,
+  padding: 4,
+  textAlign: 'center',
+};
+
+const btnStyle = {
+  position: 'absolute',
+  top: 7,
+  right: 9,
+  backgroundColor: 'transparent',
+  border: '1px solid transparent',
+  borderRadius: '50%',
+  width: '40px',
+  height: '40px',
+  transition: 'transform 0.5s, border 0.5s',
+};
+
+const Icon = {
+  fontSize: '20px',
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+};
+
+const btnHoverStyle = {
+  '&:hover': {
+    transform: 'rotate(180deg)',
+    border: '1px solid black',
+  },
+};
+// modal styles END==
 
 const Resources = () => {
-
-    // modal
-    const [open, setOpen] = useState(true);
-    const handleClose = () => setOpen(false);
-    const modalStyle = {
-        position: 'absolute',
-        minWidth: 340,
-        top: '50%',
-        left: '50%',
-        transform: 'translate(-50%, -50%)',
-        bgcolor: 'background.paper',
-        border: '2px solid #000',
-        boxShadow: 24,
-        p: 5,
-    };
-    // modal END==
-
+  const [open, setOpen] = useState(true);
+  const handleClose = () => setOpen(false);
+  
   return (
     <Styles>
       <RecommendedBooks />
@@ -32,19 +60,23 @@ const Resources = () => {
       <Footer />
 
       <Modal
-            open={open}
-            onClose={handleClose}
-            aria-labelledby="modal-modal-title"
-            aria-describedby="modal-modal-description"
-          >
-            <Box sx={modalStyle}>
-              <h4>Welcome to our Resource page!</h4>
-              <br></br>
-              <h5 style={{ textTransform: 'none' }}>Here you will find a handpicked selection 
-               of recommended books, and useful apps to support
-               you on your learning journey. Happy learning!</h5>
-            </Box>
-          </Modal>
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={modalStyle}>
+          <Box sx={{ ...btnStyle, ...btnHoverStyle }} onClick={handleClose}>
+            <RxCross1 style={Icon}/>
+          </Box>
+          <h4 style={{ fontSize: '32px' }} >Welcome to our Resource page!</h4>
+          <br />
+          <h5 style={{ textTransform: 'none' }}>
+            Here you will find a handpicked selection of recommended books, and useful apps to
+            support you on your learning journey!
+          </h5>
+        </Box>
+      </Modal>
     </Styles>
   );
 };
