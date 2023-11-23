67f25346bbb955114384af14faf00fd54ad0755a
miguel mateo <mmateo95@hotmail.com>
2023-11-22 19:11:36 -0500

    port.env added

--- server/server.js
+++ server/server.js
@@ -352,6 +352,7 @@ app.post("/send_recovery_email", (req, res) => {
       .catch((error) => res.status(500).send(error.message));
 });
 
-app.listen(8000, () => {
-    console.log('server started')
-})
+const port = process.env.PORT || 8000;
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+});
